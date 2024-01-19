f2d9f52a9bab3971c60b52a22a835efb72f8eeff
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-01-19 21:48:38 +0100

    Update answers.py

--- answers.py
+++ answers.py
@@ -3,8 +3,10 @@ class Answers:
         self._correct_answer = correct_answer
         self._wrong_answers = wrongs_answers
 
-    def get_correct_answer(self) -> str:
+    #function for correct answer 
+        def get_correct_answer(self) -> str:
         return self._correct_answer
 
-    def get_wrong_answers(self) -> tuple:
+    #function for wrong answer 
+        def get_wrong_answers(self) -> tuple:
         return self._wrong_answers
