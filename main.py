64f135bfbacef7971d02e98a49fcb30153fdfe70
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-01-21 21:56:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from game import WhoWantsToBeAMillionaire
 from question_service import QuestionService
 
-#this is the main page you start the program from here..
+#here is main page you start the program from here.
 if __name__ == "__main__":
     game = WhoWantsToBeAMillionaire(QuestionService(Path("demo_questions.txt")))
     sys.exit(game.start_game())
