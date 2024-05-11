7a2792916e4a81956263de88d9cfac9d8c0d9149
Kiara <72391175+ohansfav@users.noreply.github.com>
2024-05-11 15:48:26 +0100

    Update string_helper.py

--- string_helper.py
+++ string_helper.py
@@ -1,6 +1,6 @@
 from ansi_color_codes import Color
 
-#function for color string
+#function for color string...
 def color_string(string: str, color: str) -> str:
     return f"{color}{string}{Color.END}"
-#imported to main python file 
+#imported to main python file...
